600a3c4ed6a4032a8b041617ddad313fdcc9d6a6
eebbesen <eebbesen.git@gmail.com>
2019-01-27 16:23:58 -0600

    removing upsert

--- app/services/processor/saint_paul.rb
+++ app/services/processor/saint_paul.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'upsert/active_record_upsert'
-
 module Processor
   class SaintPaul
     URL = 'https://stpaul.legistar.com/'
@@ -69,12 +67,12 @@ module Processor
     private_class_method def self.persist_meeting(data)
       d = data.clone
       d[:date] = parse_date(d[:date])
-      Meeting.where(name: d[:name], date: data[:date]).first_or_create(d)
+      Meeting.where(name: d[:name], date: data[:date]).first_or_create!(d)
     end
 
     private_class_method def self.persist_item(data)
       d = data.clone
-      Item.where(meeting_id: d[:meeting_id], name: d[:name]).first_or_create(d)
+      Item.where(meeting_id: d[:meeting_id], name: d[:name]).first_or_create!(d)
     end
 
     private_class_method def self.parse_date(date)
