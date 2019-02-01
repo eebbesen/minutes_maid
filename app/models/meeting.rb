7de1036711e980dabf08a4357440dce23aaafa43
eebbesen <eebbesen.git@gmail.com>
2019-01-31 22:21:49 -0600

    filtering meetings

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,4 +2,15 @@
 
 class Meeting < ApplicationRecord
   has_many :items
+
+  def hclass
+    case name
+    when /^Board/
+      'water'
+    when nil
+      ''
+    else
+      name.split(' ').first.downcase
+    end
+  end
 end
