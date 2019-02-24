1cda911af618cc48e3cf7c0ece8dd094e5c419ab
eebbesen <eebbesen.git@gmail.com>
2019-02-24 15:05:07 -0600

    specify city for Heroku deployment

--- app/services/mappy.rb
+++ app/services/mappy.rb
@@ -5,13 +5,14 @@ require 'google-maps'
 # Google Maps helpser
 module Mappy
   URL = 'https://www.google.com/maps/search/?api=1&query=Google&query_place_id='
+  POST = ' Saint Paul, MN'
   class << self
     def link(place)
       places = []
       return '' if place.blank?
 
       begin
-        places = Google::Maps.places(place)
+        places = Google::Maps.places(place + POST)
       rescue Google::Maps::ZeroResultsException => zre
         puts "Exception in Mappy#link:\n#{zre.message}"
         return ''
