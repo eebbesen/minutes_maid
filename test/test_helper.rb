13363a1dbba2d6f2531d55951caa96a68afe9a62
eebbesen <eebbesen.git@gmail.com>
2019-05-07 20:09:52 -0500

    tidying up testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter 'app/channels/application_cable/channel.rb'
+  add_filter 'app/channels/application_cable/connection.rb'
+  add_filter 'app/jobs/application_job.rb'
+  add_filter 'app/mailers/application_mailer.rb'
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
