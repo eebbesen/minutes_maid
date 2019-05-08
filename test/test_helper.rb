bb1675fda79f8863c7f159d66b390127cf78d22c
eebbesen <eebbesen.git@gmail.com>
2019-05-08 07:23:43 -0500

    chromedriver-helper to webdriver

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,7 +17,9 @@ VCR.configure do |config|
   config.ignore_localhost = true
   config.cassette_library_dir = 'vcr_cassettes'
   config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
 end
+Webdrivers::Chromedriver.required_version = '2.46'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
