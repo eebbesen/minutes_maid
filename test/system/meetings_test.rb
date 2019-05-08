74ea131bdc17c25a93a9693919c0817c316c25fe
eebbesen <eebbesen.git@gmail.com>
2019-05-07 21:23:31 -0500

    more cassettes

--- test/system/meetings_test.rb
+++ test/system/meetings_test.rb
@@ -8,14 +8,14 @@ class MeetingsTest < ApplicationSystemTestCase
   end
 
   test 'visiting the index' do
-    VCR.use_cassette('meetings_1') do
+    VCR.use_cassette('meetings_system_1') do
       visit meetings_url
       assert_selector 'h1', text: 'Meetings'
     end
   end
 
   test 'filter city council' do
-    VCR.use_cassette('notes_2') do
+    VCR.use_cassette('meetings_system_2') do
       visit meetings_url
       assert_equal 4, page.all(:css, 'tr.data').size
       find(:css, '#meeting-filter').find(:option, 'City Council').select_option
