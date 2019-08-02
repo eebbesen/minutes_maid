9c347190707b8031abfed721650c88de10b8c97f
J2EEbbesen <eebbesen@users.noreply.github.com>
2019-08-01 20:30:12 -0500

    adding about page (#63)

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test 'should get about' do
+    get static_pages_about_url
+    assert_response :success
+  end
+
+end
