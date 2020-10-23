75e732c01be6be2a7cb3a1cbbd7b34d4194a01a3
eebbesen <eebbesen.git@gmail.com>
2020-10-23 06:40:26 -0500

    replacing deprecated key with slug

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,7 +29,7 @@ VCR.configure do |config|
 end
 
 # this key is not valid but referenced by VCR cassettes
-ENV['MM_GOOGLE_API_KEY'] = 'AIzaSyCTQ7QGkpvV8mw_6HDfxw-4_Sd7vNsJ6G0'
+ENV['MM_GOOGLE_API_KEY'] = 'ABCDEF'
 Google::Maps.configure do |config|
   config.authentication_mode = Google::Maps::Configuration::API_KEY
   config.api_key = ENV['MM_GOOGLE_API_KEY']
